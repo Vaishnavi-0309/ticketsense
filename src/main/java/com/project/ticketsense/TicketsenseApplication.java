b7e5c152c1f1f2414a3bfe25b6fa0cbd68e1ae7e
Vaishnavi-0309 <udipivaishnavi0309@gmail.com>
2026-08-29 21:55:42 +0530

    created ticket sense project

---
+++ src/main/java/com/project/ticketsense/TicketsenseApplication.java
@@ -0,0 +1,13 @@
+package com.project.ticketsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ticketsense/TicketsenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ticketsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
